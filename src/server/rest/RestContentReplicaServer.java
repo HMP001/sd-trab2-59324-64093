fa46e5c40fc1cc0749496c2cb95ed62b29c7f85b
Humberto Perez <cd@LAPTOP-9TIJGOUK.localdomain>
2025-06-05 09:57:05 +0100

    adding replica

---
+++ src/server/rest/RestContentReplicaServer.java
@@ -0,0 +1,59 @@
+package server.rest;
+
+import client.ContentClient;
+import network.ServiceAnnouncer;
+import server.SharedSecret;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+public class RestContentReplicaServer {
+
+    private static final Logger log = Logger.getLogger(RestContentsServer.class.getName());
+
+    static {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%n");
+    }
+
+    public static final int PORT = 8082;
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            log.severe("Missing shared secret");
+            System.exit(1);
+        }
+        SharedSecret.setSharedSecret(args[0]);
+        log.info("Using Content Replica Rest Server secret: " + args[0]);
+        launchServer(PORT);
+    }
+
+    public static void launchServer(int port) {
+        launchServer(port, Optional.empty());
+    }
+
+    public static void launchServer(int port, long period) {
+        launchServer(port, Optional.of(period));
+    }
+
+    private static void launchServer(int port, Optional<Long> period) {
+        try {
+            var serverURI = RestServerUtils.computeServerUri(port);
+            announceService(period, serverURI);
+            RestServerUtils.launchResource(serverURI, ContentResourceModified.class);
+            log.info(String.format("%s Server replica ready @ %s\n",  ContentClient.SERVICE, serverURI));
+        } catch( Exception e) {
+            log.severe(e.getMessage());
+        }
+    }
+
+    private static void announceService(Optional<Long> period, String serverURI) throws IOException {
+        if (period.isPresent())
+            new ServiceAnnouncer(ContentClient.SERVICE, serverURI, period.get());
+        else
+            new ServiceAnnouncer(ContentClient.SERVICE, serverURI);
+    }
+
+
+}
